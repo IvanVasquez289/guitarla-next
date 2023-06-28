8017ab8ba8c6df24835792b5518c9cac3852761c
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-06-27 23:43:12 -0600

    Resaltar pagina

--- components/header.js
+++ components/header.js
@@ -1,24 +1,40 @@
 import Image from "next/future/image"
 import Link from "next/link"
 import styles from '../styles/header.module.css'
+import { useRouter } from "next/router"
 const Header = () => {
+  const router = useRouter()
+  console.log(router)
   return (
     <header className={styles.header}>
         <div className={`contenedor ${styles.barra}`}>
-            <Image src='/img/logo.svg' width={200} height={10} alt="imagen logo"/>
+
+            <Link href={'/'}>
+                <a>
+                    <Image src='/img/logo.svg' width={200} height={10} alt="imagen logo"/>
+                </a>
+            </Link>
 
             <nav className={styles.navegacion}>
                 <Link href={'/'}>
-                    Inicio
+                    <a className={router.pathname == "/" ? styles.active : ''}>
+                        Inicio
+                    </a>
                 </Link>
                 <Link href={'/nosotros'}>
-                    Nosotros
+                    <a className={router.pathname == "/nosotros" ? styles.active : ''}>
+                        Nosotros
+                    </a>
                 </Link>
                 <Link href={'/tienda'}>
-                    Tienda
+                    <a className={router.pathname == "/tienda" ? styles.active : ''}>
+                        Tienda
+                    </a>
                 </Link>
                 <Link href={'/blog'}>
-                    Blog
+                    <a className={router.pathname == "/blog" ? styles.active : ''}>
+                        Blog
+                    </a>
                 </Link>
             </nav>
         </div>
