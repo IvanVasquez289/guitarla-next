0c6d411b319d6e14ff12c23ab48a5c2fdbbca810
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-06-28 22:57:51 -0600

    Creando Nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,11 +1,29 @@
 import Layout from "../components/layout"
+import Image from "next/image"
+import styles from '../styles/nosotros.module.css'
 const Nosotros = () => {
   return (
     <Layout
         title={'Nosotros'}
         description={'Sobre nosotros'}
     >
-        <h1>Nosotros</h1>
+        <main className="contenedor">
+          <h2 className="heading">Nosotros</h2>
+
+          <div className={styles.contenido}>
+            <Image src="/img/nosotros.jpg" width={1000} height={800} alt="Imagen sobre nosotros"/>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque quasi delectus fugit molestiae ipsum pariatur officia, sint dolorem velit officiis animi eum. Fugit, quis. Reprehenderit, id! Exercitationem magni assumenda minus!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque quasi delectus fugit molestiae ipsum pariatur officia, sint dolorem velit officiis animi eum. Fugit, quis. 
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque quasi delectus fugit molestiae ipsum pariatur officia, sint dolorem velit officiis animi eum. Fugit, quis. Reprehenderit, id! Exercitationem magni assumenda minus!
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque quasi delectus fugit molestiae ipsum pariatur officia, sint dolorem velit officiis animi eum. Fugit, quis. Reprehenderit, id! 
+              </p>
+            </div>
+          </div>
+        </main>
     </Layout>
   )
 }
