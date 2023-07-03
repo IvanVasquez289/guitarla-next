ebb4c81e83cd6f42797071dee7325952f98948e9
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-03 14:46:10 -0600

    Routing Dinamico con getStaticPaths y getStaticProps

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -7,7 +7,22 @@ const Producto = (guitarra) => {
 
 export default Producto
 
-export async function getServerSideProps({query: {url}}){
+export async function getStaticPaths(){
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras`)
+  const {data} = await respuesta.json()
+  const paths = data.map(guitarra => ({
+    params: {
+      url: guitarra.attributes.url
+    }
+  }))
+  
+  return{
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params: {url}}){
   const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
   const {data} = await respuesta.json()
   const guitarra = data[0].attributes
@@ -17,3 +32,13 @@ export async function getServerSideProps({query: {url}}){
     }
   }
 }
+// export async function getServerSideProps({query: {url}}){
+//   const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`)
+//   const {data} = await respuesta.json()
+//   const guitarra = data[0].attributes
+//   return{
+//     props:{
+//       guitarra
+//     }
+//   }
+// }
