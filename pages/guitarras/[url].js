8a501b2cd1045b9a45152582033ca7954120168e
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-07-03 15:13:51 -0600

    Mostrando informacion de la guitara

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,7 +1,22 @@
+import Layout from "../../components/layout"
+import styles from "../../styles/guitarras.module.css"
+import Image from "next/future/image"
 const Producto = (guitarra) => {
-  console.log(guitarra.guitarra.nombre)
+  const {nombre,descripcion,precio,url,imagen} = guitarra.guitarra
   return (
-    <div>Producto</div>
+    <Layout
+      title={`Guitarra ${nombre}`}
+    >
+      <div className={styles.guitarra}>
+        <Image src={imagen.data.attributes.url} width={600} height={400} alt={`imagen guitarra ${nombre}`}/>
+
+        <div className={styles.contenido}>
+          <h3>{nombre}</h3>
+          <p className={styles.descripcion}>{descripcion}</p>
+          <p className={styles.precio}>{precio}</p>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
